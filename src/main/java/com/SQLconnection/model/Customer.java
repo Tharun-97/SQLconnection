63b7e320a0b2d14abce9c533da8c19e293f294f6
Tharunkandasaami D <kandasaamitharun97@gmail.com>
2022-11-21 17:01:43 +0530

    Update Customer.java

--- src/main/java/com/SQLconnection/model/Customer.java
+++ src/main/java/com/SQLconnection/model/Customer.java
@@ -5,48 +5,50 @@ public class Customer {
 	private int custNo;
 	private String custName;
 	private String country;
-	
-	
-	
+
 	public Customer() {
 
 	}
+
 	public Customer(int custNo, String custName, String country) {
-		System.out.println(custNo+custName+country);
+
 		this.custNo = custNo;
 		this.custName = custName;
 		this.country = country;
 	}
+
 	public Customer(String custName, String country) {
 		this.custName = custName;
 		this.country = country;
 	}
-	
-	
+
 	public int getCustNo() {
 		return custNo;
 	}
+
 	public void setCustNo(int custNo) {
 		this.custNo = custNo;
 	}
+
 	public String getCustName() {
 		return custName;
 	}
+
 	public void setCustName(String custName) {
 		this.custName = custName;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
 
 	@Override
 	public String toString() {
-		return	"No: "+custNo+" Name: "+custName+" Country: "+country;
+		return "No: " + custNo + " Name: " + custName + " Country: " + country;
 	}
-	
-	
 
 }
